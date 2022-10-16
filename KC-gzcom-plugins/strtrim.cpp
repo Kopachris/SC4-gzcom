b6c80849d8d640a040d5740460062fe96ba8cbeb
Kopachris <kopachris@gmail.com>
2022-10-16 11:43:32 -0700

    Made some changes to eteran's string trimming example

--- KC-gzcom-plugins/strtrim.cpp
+++ KC-gzcom-plugins/strtrim.cpp
@@ -2,6 +2,11 @@
 * Trim whitespace from strings either in-place or returning a copy.
 * 
 * Originally from https://stackoverflow.com/a/217605 licensed CC-BY-SA 4.0
+* 
+* Updated to use char8_t and with versions which specify their own trim characters
+* Also made the "copy" version default and indicated in-place versions by prefixing the function names with 'i'
+* And made a header file so this can be included in other programs
+* 
 */
 
 #include "strtrim.h"
@@ -9,40 +14,81 @@
 #include <cctype>
 #include <locale>
 
-// trim from start (in place)
-static inline void strtrim::ltrim(std::string& s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-        return !std::isspace(ch);
-        }));
-}
+namespace strtrim {
+    // trim from start (in place)
+    static void iltrim(std::string& s) {
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](char8_t ch) {
+            return !std::isspace(ch);
+            }));
+    }
 
-// trim from end (in place)
-static inline void strtrim::rtrim(std::string& s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-        return !std::isspace(ch);
-        }).base(), s.end());
-}
+    // trim from end (in place)
+    static void irtrim(std::string& s) {
+        s.erase(std::find_if(s.rbegin(), s.rend(), [](char8_t ch) {
+            return !std::isspace(ch);
+            }).base(), s.end());
+    }
 
-// trim from both ends (in place)
-static inline void strtrim::trim(std::string& s) {
-    ltrim(s);
-    rtrim(s);
-}
+    // trim any of selected chars instead of whitespace
+    static void iltrim(std::string& s, std::string trim_chars)
+    {
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [trim_chars](char8_t ch) {
+            return !trim_chars.contains(ch);
+            }));
+    }
 
-// trim from start (copying)
-static inline std::string strtrim::ltrim_copy(std::string s) {
-    ltrim(s);
-    return s;
-}
+    static void irtrim(std::string& s, std::string trim_chars)
+    {
+        s.erase(std::find_if(s.rbegin(), s.rend(), [trim_chars](char8_t ch) {
+            return !trim_chars.contains(ch);
+            }).base(), s.end());
+    }
 
-// trim from end (copying)
-static inline std::string strtrim::rtrim_copy(std::string s) {
-    rtrim(s);
-    return s;
-}
+    // trim from both ends (in place)
+    static void itrim(std::string& s) {
+        iltrim(s);
+        irtrim(s);
+    }
 
-// trim from both ends (copying)
-static inline std::string strtrim::trim_copy(std::string s) {
-    trim(s);
-    return s;
+    static void itrim(std::string& s, std::string trim_chars)
+    {
+        iltrim(s, trim_chars);
+        irtrim(s, trim_chars);
+    }
+
+    // trim from start (copying)
+    static std::string ltrim(std::string s) {
+        iltrim(s);
+        return s;
+    }
+
+    static std::string ltrim(std::string s, std::string trim_chars)
+    {
+        iltrim(s, trim_chars);
+        return s;
+    }
+
+    // trim from end (copying)
+    static std::string rtrim(std::string s) {
+        irtrim(s);
+        return s;
+    }
+
+    static std::string rtrim(std::string s, std::string trim_chars)
+    {
+        irtrim(s, trim_chars);
+        return s;
+    }
+
+    // trim from both ends (copying)
+    static std::string trim(std::string s) {
+        itrim(s);
+        return s;
+    }
+
+    static std::string trim(std::string s, std::string trim_chars)
+    {
+        itrim(s, trim_chars);
+        return s;
+    }
 }
--- KC-gzcom-plugins/strtrim.h
+++ KC-gzcom-plugins/strtrim.h
@@ -1,11 +1,28 @@
 #pragma once
 
+/*
+* Trim whitespace from strings either in-place or returning a copy.
+*
+* Originally from https://stackoverflow.com/a/217605 licensed CC-BY-SA 4.0
+*
+* Updated to use char8_t and with versions which specify their own trim characters
+* Also made the "copy" version default and indicated in-place versions by prefixing the function names with 'i'
+* And made a header file so this can be included in other programs
+*
+*/
+
 namespace strtrim
 {
-	static inline void ltrim(std::string& s);
-	static inline void rtrim(std::string& s);
-	static inline void trim(std::string& s);
-	static inline std::string ltrim_copy(std::string s);
-	static inline std::string rtrim_copy(std::string s);
-	static inline std::string trim_copy(std::string s);
+	static void iltrim(std::string& s);
+	static void iltrim(std::string& s, std::string trim_chars);
+	static void irtrim(std::string& s);
+	static void irtrim(std::string& s, std::string trim_chars);
+	static void itrim(std::string& s);
+	static void itrim(std::string& s, std::string trim_chars);
+	static std::string ltrim(std::string s);
+	static std::string ltrim(std::string s, std::string trim_chars);
+	static std::string rtrim(std::string s);
+	static std::string rtrim(std::string s, std::string trim_chars);
+	static std::string trim(std::string s);
+	static std::string trim(std::string s, std::string trim_chars);
 }
